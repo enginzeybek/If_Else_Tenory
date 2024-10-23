a6de723c5e97b86f94a3590c1ce7caad357b58fa
Engin Zeybek <engin_zeybek.tb@hotmail.com>
2024-10-24 01:04:24 +0300

    ilk commit

---
+++ If_Else_Tenory/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int time = DateTime.Now.Hour;
+
+if (time <= 18)
+{
+    Console.WriteLine("İYİ GÜNLER");
+}
+else if (time <= 6)
+{
+    Console.WriteLine("İYİ SABAHLAR");
+}
+else if (time <= 24)
+{
+    Console.WriteLine("İYİ AKŞAMLAR");
+}
+
+String sonuc = time <= 18 ? "İYİ GÜNLER" : "İYİ GECELER";
+Console.WriteLine(sonuc);
